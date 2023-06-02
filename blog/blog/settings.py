ea8d7f712d4ea75e7804abc15fe1dc22dd742c33
左汉明 <zhm@100yueoo.com>
2023-06-02 11:09:44 +0800

    数据库配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT':'3306',
+        'USER':'zhm',
+        'PASSWORD':'123456',
+        'NAME':'blog'    },
 }
 
 
